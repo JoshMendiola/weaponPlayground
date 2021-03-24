31ef8bc9bf67a361394c872ae3c16f3b488d370e
PhilosophyWithJosh <joshmendiola@icloud.com>
2021-03-24 12:08:24 -0500

    work in progress

--- weaponPlayground/src/me/PhilosophyWithJosh/weaponPlayground/listeners/joshDamageListener.java
+++ weaponPlayground/src/me/PhilosophyWithJosh/weaponPlayground/listeners/joshDamageListener.java
@@ -1,6 +1,7 @@
 package me.PhilosophyWithJosh.weaponPlayground.listeners;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,15 +11,14 @@ import me.PhilosophyWithJosh.weaponPlayground.utils.utils;
 
 public class joshDamageListener implements Listener
 {
-	
-	private Main plugin;
+	private static Main plugin;
 	
 	public joshDamageListener(Main plugin)
 	{
 		this.plugin = plugin;
 		Bukkit.getPluginManager().registerEvents(this, plugin);
 	}
-	//this sends a message to any players who are taking damage from me
+	//this sends a message to any players who are taking damage from me, or are damaging me, just Joshua damage in general
 	@EventHandler
 	public void onJoshDamage(EntityDamageByEntityEvent d)
 	{
@@ -26,29 +26,29 @@ public class joshDamageListener implements Listener
 		if (d.getEntity() instanceof Player && d.getDamager() instanceof Player)
 		{
 			
-			Player p = (Player)d.getEntity();
-			Player a = (Player)d.getDamager();
-			if(a.getDisplayName() == "PhilosophyWithJ")
+			Player p = (Player) d.getEntity();
+			Player a = (Player) d.getDamager();
+			if(a.getDisplayName().equals("PhilosophyWithJ"))
 			{
 				//runs it 3 times
 				for(int i = 0; i < 3; i++)
 				{
-					p.sendMessage(utils.chat("&cOBEY"));
-					++i;			
+					p.sendMessage(utils.chat("&cOBEY"));			
 				}
 			}
 			//sends damage to all players if one player hurts Josh(the king)
-			else if(p.getDisplayName() == "PhilosophyWithJ")
+			else if(p.getDisplayName().equals("PhilosophyWithJ"))
 			{
 				for(Player everyP: Bukkit.getServer().getOnlinePlayers())
 				{
 					//provides blame for all players to see in the case they receive damage from hurting Josh, and then damages all of them
-					if(everyP.getHealth() > 0&&everyP.getDisplayName()!="PhilosophyWithJ")
+					if(everyP instanceof LivingEntity && !everyP.getDisplayName().equals("PhilosophyWithJ"))
 					{
-						everyP.damage(1.0);
-						Bukkit.broadcastMessage(utils.chat("&a" + a.getDisplayName() + "&7 is hurting the king!"));
+						LivingEntity victim = (LivingEntity)everyP;
+						victim.damage(1.0);
 					}
 				}
+				Bukkit.broadcastMessage(utils.chat("&a" + a.getDisplayName() + "&7 is hurting the king!"));
 			}
 			
 		}
